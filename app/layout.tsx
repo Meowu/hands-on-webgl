a5898831c880f5caf9969fd7ae2a6943143be639
Meowu <474384902@qq.com>
2023-05-30 02:44:37 +0800

    fix styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,13 +14,13 @@ export const metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   console.log("props: ", inter.className);
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <script src="/lib/cuon-matrix.js" async />
       <script src="/lib/cuon-utils.js" async />
       <script src="/lib/webgl-debug.js" async />
       <script src="/lib/webgl-utils.js" async />
-      <body className={inter.className}>
-        <div className="flex flex-col h-full min-h-screen flex-nowrap">
+      <body className={inter.className + ' h-full min-h-screen'}>
+        <div className="flex flex-col h-full flex-nowrap">
           <NavBar />
           <div className="px-12 py-6">{props.children}</div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div className="grid mb-32 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         Home.
       </div>

746bb53fcdc9046b51cdfc8612279a7865c9f910
Meowu <474384902@qq.com>
2023-06-21 13:33:09 +0800

    add LightedCube_ambient

--- app/chapters/08/page.tsx
+++ app/chapters/08/page.tsx
@@ -3,6 +3,7 @@
 import Card from '../../components/Card'
 import LightedCube from './LightedCube'
 import LightedCube_animation from './LightedCube_animation'
+import LightedCube_ambient from './LightedCube_ambient'
 
 export default function Chapter() {
     return <div className="p-6">
@@ -13,9 +14,9 @@ export default function Chapter() {
             <Card name={LightedCube_animation.name}>
                 <LightedCube_animation />
             </Card>
-            {/* <Card name={HelloPoint2.name}>
-                <HelloPoint2 />
-            </Card> */}
+            <Card name={LightedCube_ambient.name}>
+                <LightedCube_ambient />
+            </Card>
             {/* <Card name={ClickedPoints.name}>
                 <ClickedPoints />
             </Card> */}
